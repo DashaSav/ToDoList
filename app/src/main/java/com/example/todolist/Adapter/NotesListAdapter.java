fc866bb022255e7ecaee4599daf82faa55e94dca
DashaSav <savenkodasa772@gmail.com>
2022-11-08 20:29:24 +0300

    skeleton

---
+++ app/src/main/java/com/example/todolist/Adapter/NotesListAdapter.java
@@ -0,0 +1,16 @@
+package com.example.todolist.Adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotesListAdapter {
+
+
+}
+class NotesViewHolder extends RecyclerView.ViewHolder {
+    public NotesViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
---
+++ app/src/main/java/com/example/todolist/DataBase/mainDAO.java
@@ -0,0 +1,26 @@
+package com.example.todolist.DataBase;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.todolist.models.Note;
+
+import java.util.List;
+
+@Dao
+public interface mainDAO {
+    @Insert (onConflict = REPLACE)
+    void insert (Note notes);
+    @Query("SELECT * FROM notes ORDER BY id DESC")
+    List<Note> getAll();
+    @Query("UPDATE notes SET title = :title, notes = :notes WHERE ID = :id")
+    void update(int id, String title, String notes);
+
+    @Delete
+    void delete (Note note);
+
+}
