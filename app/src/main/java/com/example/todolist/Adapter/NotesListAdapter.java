18f24c448c33ec6f942ed44c79af72b524182c3a
DashaSav <savenkodasa772@gmail.com>
2022-11-12 14:43:15 +0300

    Adapter added, viewHolder implemented

--- app/src/main/java/com/example/todolist/Adapter/NotesListAdapter.java
+++ app/src/main/java/com/example/todolist/Adapter/NotesListAdapter.java
@@ -1,16 +1,48 @@
 package com.example.todolist.Adapter;
 
+import android.media.Image;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NotesListAdapter {
+import com.example.todolist.R;
+
+public class NotesListAdapter extends RecyclerView.Adapter<NotesViewHolder>{
 
 
+    @NonNull
+    @Override
+    public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
 class NotesViewHolder extends RecyclerView.ViewHolder {
+    CardView notes_container;
+    TextView textView_title,textView_notes, textView_date;
+    ImageView imageView_pin;
+
+
     public NotesViewHolder(@NonNull View itemView) {
         super(itemView);
+        notes_container = itemView.findViewById(R.id.notes_container);
+        textView_title = itemView.findViewById(R.id.textView_title);
+        textView_notes = itemView.findViewById(R.id.textView_notes);
+        textView_date = itemView.findViewById(R.id.textView_date);
+        imageView_pin = itemView.findViewById(R.id.imageView_pin);
     }
 }
